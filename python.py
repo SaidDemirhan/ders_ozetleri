e82043424a2fffc14b7816f25545f40a1c06e94e
said <saiddemirhan@gmail.com>
2021-12-05 13:52:38 +0100

    v4

---
+++ python.py
@@ -0,0 +1,8 @@
+print("Hosgeldiniz!")
+isim="ali"
+soyisim="can"
+print("Ad: ",isim,"\nSoyad: ",soyisim)
+rakamalar=[1,2,3,4]
+print(type(rakamalar))#list
+esyalar=("kalem","masa","kitap")
+print(type(esyalar))#<class 'tuple'>
