0290a34664e3abbb605a82266c1903a1afe961d1
said <saiddemirhan@gmail.com>
2022-05-24 15:39:10 +0200

    js

---
+++ JavaScript/script.js
@@ -0,0 +1,38 @@
+//alert("Hos Geldiniz");
+console.log("Herzlich Willkommen");
+//sayfa girisi bilgilendirme uyarisi
+
+console.log(123);
+console.error("hata");
+console.warn("ACHTUNG");
+//sayfanin incele kismindaki konsol kismindan bu ayrintilar görünür
+
+//console.clear
+//konsoldaki yazlari temizler
+
+//////////////////////////////////////////////////////////////////
+//Degiskenler
+
+var adSoyad = "Said Demirhan";
+
+console.log(adSoyad);
+
+yas=25;
+console.log(yas);
+
+//degisken isimleri rakamla baslayamaz. - 1yas
+//yasak kelimeleri degisken isimlerinde kullanamyiz var if="kk"
+//degisken adinda bosluk birakilamaz - adSoyad
+//var her zaman degistirilebilir
+
+const eMail = "saiddemirhan@gmail.com"
+console.log(eMail);
+//const hicbir zaman degistirilemez.
+
+//////////////////////////////////////////////////////////////////
+//Degisken türleri
+    //Primitive
+    //String
+
+    let isim="Said";
+    console.log(typeof isim)
